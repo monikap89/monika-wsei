389e16691e5914eaf7ca57812c3053842526af21
monikap89 <moka_org@wp.pl>
2020-03-18 19:51:50 +0100

    1st commit update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 
-float waga, wzrost, wynik;
+float waga, wzrost, wynik, analiza;
 float bmi(float waga, float wzrost);
+float wart(float analiza);
 
 using namespace std;
 
@@ -19,18 +20,31 @@ int main() {
     wynik = bmi(waga, wzrost);
 
     cout << "\t\tTwoje BMI wynosi: " << wynik << endl;
-
+    cout << endl;
+    cout << wart(analiza);
     cout << endl;
     cout << endl;
     cout << "\t\t\t=== KONIEC PROGRAMU ===" << endl;
     cout << endl;
     cout << endl;
-    system("pause");
 
+    system("pause");
     return 0;
 }
 
 float bmi(float waga, float wzrost)
 {
     return waga/((wzrost/100)*(wzrost/100)) ;
+}
+
+float wart(float analiza)
+{
+    if (wynik<18)
+        cout << "\t\tWazysz za malo, sprobuj troche przytyc! :)" << endl;
+
+    else if (18.5>=wynik<=24.9)
+        cout << "\t\tWazysz prawidlowo. Tak trzymaj! :)" << endl;
+
+    else
+        cout << "\t\tWazysz zbyt duzo. Musisz o siebie zadbac!" << endl;
 }
